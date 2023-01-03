156e61c66a92d6d8ccb7fc9d47b0f74687672d14
Raghu K <raghuk.vit@gmail.com>
2023-01-03 06:57:04 +0530

    Shell Scripting Topics

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,21 @@
 echo Hello World
 
 echo "**** Hello World ****"
+
+# Color COde Syntax : echo -e "\e[COLmMESSAGE\[0m"
+# Red       - 31 - echo -e "\e[31mMESSAGE\[0m"
+# Green     - 32 - echo -e "\e[32mMESSAGE\[0m"
+# Yellow    - 33 - echo -e "\e[33mMESSAGE\[0m"
+# Blue      - 34 - echo -e "\e[34mMESSAGE\[0m"
+# Magenta   - 35 - echo -e "\e[35mMESSAGE\[0m"
+# Cyan      - 36 - echo -e "\e[36mMESSAGE\[0m"
+
+echo -e "\e[31mMESSAGE\[0m"
+echo -e "\e[32mMESSAGE\[0m"
+echo -e "\e[33mMESSAGE\[0m"
+echo -e "\e[34mMESSAGE\[0m"
+echo -e "\e[35mMESSAGE\[0m"
+echo -e "\e[36mMESSAGE\[0m"
+
+
+
